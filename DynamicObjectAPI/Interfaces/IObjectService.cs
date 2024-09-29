81f3577f676de3f66d7a4e946181204bcdcafed4
hilal <hilallgungoorr@gmail.com>
2024-09-29 21:54:03 +0300

    Refactor for dynamic object creation

--- DynamicObjectAPI/Interfaces/IObjectService.cs
+++ DynamicObjectAPI/Interfaces/IObjectService.cs
@@ -1,13 +1,14 @@
 ﻿using DynamicObjectAPI.Models;
 using DynamicObjectAPI.Request;
 using DynamicObjectAPI.Response;
+using System.Dynamic;
 
 namespace DynamicObjectAPI.Interfaces
 {
     public interface IObjectService
     {
         Task<ResponseViewModel<object>> CreateObjectAsync(CreateObjectRequest request);
-        Task<ResponseViewModel<DynamicObjectResponse>> GetObjectAsync(string type, int id);
+        Task<ResponseViewModel<Dictionary<string, object>>> GetObjectAsync(string type, int id);
         Task<ResponseViewModel<object>> UpdateObjectAsync(int id, UpdateObjectRequest request);
         Task<ResponseViewModel<object>> DeleteObjectAsync(int id);
 
---
+++ DynamicObjectAPI/Models/DynamicObjectTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicObjectAPI.Models
+{
+    public class DynamicObjectTypes
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } 
+        public DateTime CreateDate { get; set; }
+
+    }
+}
